898841bc6b7afa064fef3f15e4ec79d3978f6c7c
Lee <lee@happygastropod.com>
2011-02-28 14:43:46 -0800

    Fixed i/o hang when accessing xclip on Linux.

--- lib/boom/platform.rb
+++ lib/boom/platform.rb
@@ -45,8 +45,7 @@ module Boom
       def copy(item)
         copy_command = darwin? ? "pbcopy" : "xclip -selection clipboard"
 
-        `echo '#{item.value.gsub("\'","\\'")}' | tr -d "\n" | #{copy_command}`
-
+        system("echo '#{item.value}' | tr -d '\n' | #{copy_command}")
         "Boom! We just copied #{item.value} to your clipboard."
       end
     end
